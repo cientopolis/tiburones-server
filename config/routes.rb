1b2f2fedf2fd79999b60f9177899b26087e4b742
WilliamGaggiotti <gaggiottiwilliam@gmail.com>
2018-05-30 12:36:29 -0300

    login subirArchivo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/edit'
   get 'principal/index'
   devise_for :users
   resources :base
-  root 'principal#index'
-  #root '/devise/sessions#new'
+  resources :posts
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_125020) do
+ActiveRecord::Schema.define(version: 2018_05_30_144053) do
 
   create_table "bases", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
